878ce6e98cf940b9f787d96a1643f22436ed5e29
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-09-20 15:30:22 -0400

    feat(client): ✨ add firebase to client

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -2,8 +2,8 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		command: 'npm --prefix .. run emulators:preview',
+		port: 5000
 	}
 };
 
