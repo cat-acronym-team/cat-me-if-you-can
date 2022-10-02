de35f697c9ffa5baee9422146574aa2467a2c375
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-10-02 16:50:16 -0400

    feat(functions): ✨ add prompt files
    I am using json because it is easy to import and it can be copied by typescript.

---
+++ functions/tests/prompts.test.ts
@@ -0,0 +1,27 @@
+import { catagories, promptsByCategory } from "../src/prompts";
+
+import { expect } from "chai";
+
+describe("prompts", () => {
+  it("should have at least 3 questions in every category", () => {
+    for (const category of catagories) {
+      expect(promptsByCategory[category].length).to.be.greaterThanOrEqual(3);
+    }
+  });
+
+  it("should only have prompts that start with a capital letter and end with a question mark", () => {
+    for (const category of catagories) {
+      for (const prompt of promptsByCategory[category]) {
+        expect(prompt).to.match(/^[A-Z].*\?$/);
+      }
+    }
+  });
+
+  it("should not have any duplicate prompts", () => {
+    for (const category of catagories) {
+      const prompts = promptsByCategory[category];
+      const uniquePrompts = Array.from(new Set(prompts));
+      expect(prompts).to.equal(uniquePrompts);
+    }
+  });
+});
