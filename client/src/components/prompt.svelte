0ff8f45fb74b0c42396dfaf0c4cfddbea5ea389e
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-10-04 15:24:02 -0400

    feat(client): 🚧 start adding the prompt component

---
+++ client/src/components/prompt.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let prompt: string;
+</script>
+
+<div class="wraper">
+  <lable class="question" for="answer">{prompt}</lable>
+
+  <input id="answer" type="text" />
+</div>
+
+<style>
+  .wraper {
+    height: 100%;
+    display: grid;
+    grid-template-rows: 1fr 1fr 1fr;
+    place-items: center;
+  }
+
+  .question {
+    font-size: 2rem;
+    font-weight: bold;
+  }
+
+  #answer {
+    font-size: 2rem;
+    font-weight: bold;
+  }
+</style>
---
+++ client/src/routes/prompt/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Prompt from "$components/prompt.svelte";
+  import type { Lobby, PrivatePlayer } from "$lib/firebase/firestore-types/lobby";
+
+  const lobby: Lobby = {
+    uids: ["nate"],
+    players: [{ alive: true, displayName: "Nate", avatar: 1 }],
+    state: "PROMPT",
+  };
+  const privatePlayer: PrivatePlayer = {
+    role: "CAT",
+    prompt: "What is a food that you have never tried but would like to?",
+  };
+</script>
+
+<div class="wraper">
+  <Prompt prompt={privatePlayer.prompt} />
+</div>
+
+<style>
+  .wraper {
+    height: 100vh;
+  }
+</style>
