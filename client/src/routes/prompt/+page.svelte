869937522b5efc3e2dacef770842ea4b5cfd8de6
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-10-06 14:56:54 -0400

    feat(client): ✨ add uid and lobby code to temporary route

--- client/src/routes/prompt/+page.svelte
+++ client/src/routes/prompt/+page.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
   import Prompt from "$components/prompt.svelte";
+  import { auth } from "$lib/firebase/app";
+  import { onAuthStateChanged } from "firebase/auth";
+
   import type { Lobby, PrivatePlayer } from "$lib/firebase/firestore-types/lobby";
+
+  let uid = "nate";
+
+  onAuthStateChanged(auth, (user) => {
+    console.log("user", user);
+
+    if (user) {
+      uid = user.uid;
+    }
+  });
 
   const lobby: Lobby = {
     uids: ["nate"],
     players: [{ alive: true, displayName: "Nate", avatar: 1 }],
     state: "PROMPT",
   };
+
   const privatePlayer: PrivatePlayer = {
     role: "CAT",
     prompt: "What is a food that you have never tried but would like to?",
@@ -14,7 +28,7 @@
 </script>
 
 <div class="wraper">
-  <Prompt prompt={privatePlayer.prompt} />
+  <Prompt prompt={privatePlayer.prompt} {uid} lobbyCode={"aaaaaa"} />
 </div>
 
 <style>
