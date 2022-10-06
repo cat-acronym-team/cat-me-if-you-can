9ed4b444c7b789ddef565f0e46139a4bb8e7e5cc
mrjoshua520 <joshua52099@gmail.com>
2022-10-06 12:03:30 -0400

    perf(client): ⚡️ Improve performance when creating lobby code
    Improves the performance by not pulling every document that exists

--- client/src/lib/firebase/create-lobby.ts
+++ client/src/lib/firebase/create-lobby.ts
@@ -2,27 +2,33 @@ import { doc, getDocs, setDoc } from "firebase/firestore";
 import { lobbyCollection } from "./firestore-collections";
 import { auth } from "./app";
 
-export async function createLobby() {
+export async function createLobby(): Promise<string> {
   const code = await createCode(); // Creates lobby code
   const user = auth.currentUser?.uid;
 
   if (user === null || user === undefined) {
-    return; // Figure out later
+    throw new Error("No user account defined"); // TODO: Set User to Anonomous User
   } else {
-    await setDoc(doc(lobbyCollection, code), {
-      uids: [user.toString()],
-      players: [
-        {
-          alive: true,
-          avatar: 1,
-          displayName: "default",
-          // votes
-        },
-      ],
-      state: "WAIT",
-    });
+    for (let index = 0; index < 5; index++) {
+      try {
+        await setDoc(doc(lobbyCollection, code), {
+          uids: [user.toString()],
+          players: [
+            {
+              alive: true,
+              avatar: 1,
+              displayName: "default",
+            },
+          ],
+          state: "WAIT",
+        });
 
-    return code;
+        return code;
+      } catch (error) {
+        continue;
+      }
+    }
+    throw new Error("Cannot create document. Maximum number of tries exceeded");
   }
 }
 
@@ -32,18 +38,9 @@ async function createCode() {
   let code = "";
   const charactersLength = characters.length;
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 6; i++) {
     code += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
-
-  const checkCode = await getDocs(lobbyCollection);
-
-  checkCode.forEach((doc) => {
-    /* Run check to see if lobby with code already exists */
-    if (code === doc.id) {
-      createCode();
-    }
-  });
 
   return code;
 }
