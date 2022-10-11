80a4c8504260da843a85804994fc59245fd6b7f7
Muaad2001 <98790792+Muaad2001@users.noreply.github.com>
2022-10-11 10:31:27 -0400

    Signout almost done

---
+++ client/src/routes/signout/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { getAuth, signOut } from "firebase/auth";
+  const auth = getAuth();
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch((error) => {
+      // An error happened.
+    });
+</script>
+
+<button onClick={() => signOut()}>Sign out</button>
+
+<style>
+</style>
