67fe06578f705a3dadacd0a9223530e58bea83e7
gchamb <gchambe2@emich.edu>
2022-10-11 10:11:01 -0400

    refactor: removed console log in tests

--- client/tests/test.ts
+++ client/tests/test.ts
@@ -2,6 +2,5 @@ import { expect, test } from "@playwright/test";
 
 test("index page has expected h1", async ({ page }) => {
   await page.goto("/");
-  console.log(await page.content)
   expect(await page.textContent("h1")).toBe("Rules!");
 });
