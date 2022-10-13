4bad155c0b5a625e93378ca7e6fd005662ef7f7d
cmcclusky <cmcclus1@emich.edu>
2022-10-13 11:28:11 -0400

    feat(client): ✨ Create structure for receive role.
    Created a structure for the assignRole funtion, going to add documentation and fix Player errors.

---
+++ client/src/routes/receiverole/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Player, Lobby, PrivatePlayer } from "$lib/firebase/firestore-types/lobby";
+
+  var numCatFish = 0;
+  var check: number[];
+  var current = 0;
+  function assignRole() {
+    for (let i = 0; i < numCatFish; i++) {
+      current = Math.floor(Math.random() * (Player.count - 1) + 0);
+      if (check.indexOf(current) === -1) {
+        check.push(current);
+      } else i--;
+    }
+    for (let j = 0; j < Player.length; j++) {
+      if (check.indexOf(j) === -1) {
+        Player[j].Role == "CAT";
+      } else {
+        Player[j].role == "CATFISH";
+      }
+    }
+  }
+</script>
