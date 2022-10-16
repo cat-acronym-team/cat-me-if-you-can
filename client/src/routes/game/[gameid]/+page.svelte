186d1acc7e5ad6076fbcb19a4acbb34d6113acf4
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-10-16 18:36:46 -0400

    refactor(client): 🚚 rename prompt component to use a Capital letter
    this alligns with our naming conventions

--- client/src/routes/game/[gameid]/+page.svelte
+++ client/src/routes/game/[gameid]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import Prompt from "$components/prompt.svelte";
+  import Prompt from "$components/Prompt.svelte";
   import Lobby from "$components/Lobby.svelte";
 
   import { page } from "$app/stores";
