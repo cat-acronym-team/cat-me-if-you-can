b3169a035693ae8df1414838f88148c6d9977ba8
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-10-16 22:42:14 -0400

    test(functions): ✅ add tests for isLobbyRequest

---
+++ functions/tests/firestore-functions-types.test.ts
@@ -0,0 +1,33 @@
+import { isLobbyRequest } from "../src/firestore-functions-types";
+
+import { expect } from "chai";
+
+describe("isLobbyRequest", () => {
+  it("should allow valid lobby request", () => {
+    const data = {
+      code: "123456",
+    };
+
+    expect(isLobbyRequest(data)).to.be.true;
+  });
+
+  it("should not allow if code is a number", () => {
+    const data = {
+      code: 123456,
+    };
+
+    expect(isLobbyRequest(data)).to.be.false;
+  });
+
+  it("should not allow if data is an empty object", () => {
+    const data = {};
+
+    expect(isLobbyRequest(data)).to.be.false;
+  });
+
+  it("should not allow if data is a string", () => {
+    const data = "123456";
+
+    expect(isLobbyRequest(data)).to.be.false;
+  });
+});
