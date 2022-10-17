c70aee9602838625507a950cd97ba8932cb7fefe
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-10-17 12:21:57 -0400

    refactor(client): ➖ remove rxfire

--- client/src/routes/game/[gameid]/+page.svelte
+++ client/src/routes/game/[gameid]/+page.svelte
@@ -1,39 +1,30 @@
 <script lang="ts">
   import Prompt from "$components/Prompt.svelte";
-  import Lobby from "$components/Lobby.svelte";
+  import LobbyComponent from "$components/Lobby.svelte";
 
   import { page } from "$app/stores";
-  import { getPrivatePlayerCollection, lobbyCollection } from "$lib/firebase/firestore-collections";
-  import { doc } from "firebase/firestore";
-  import { docData } from "rxfire/firestore";
-  import { auth } from "$lib/firebase/app";
-  import { authState } from "rxfire/auth";
-  import { mergeMap, filter, type Observable } from "rxjs";
   import type { User } from "firebase/auth";
+  import type { Lobby, PrivatePlayer } from "$lib/firebase/firestore-types/lobby";
 
   const lobbyCode = $page.params.gameid;
 
-  const lobbyDocRef = doc(lobbyCollection, lobbyCode);
+  let user: User; // TODO
 
-  const lobby$ = docData(lobbyDocRef);
+  let lobby: Lobby; // TODO
 
-  const user$ = authState(auth).pipe(filter((user) => user != null)) as Observable<User>;
-
-  const privatePlayerCollection = getPrivatePlayerCollection(lobbyDocRef);
-
-  const privatePlayer$ = user$.pipe(mergeMap((user) => docData(doc(privatePlayerCollection, user.uid))));
+  let privatePlayer: PrivatePlayer; // TODO
 </script>
 
 <div>
-  {#if $user$ == undefined || $lobby$ == undefined}
+  {#if user == undefined || lobby == undefined}
     Loading... <!-- TODO: make a Nice Loading spinner -->
-  {:else if $lobby$.state === "WAIT"}
-    <Lobby {lobbyCode} lobby={$lobby$} />
-  {:else if $privatePlayer$ == undefined}
+  {:else if lobby.state === "WAIT"}
+    <LobbyComponent {lobbyCode} {lobby} />
+  {:else if privatePlayer == undefined}
     Loading... <!-- TODO: make a Nice Loading spinner -->
-  {:else if $lobby$.state === "PROMPT"}
-    <Prompt prompt={$privatePlayer$.prompt} uid={$user$.uid} {lobbyCode} />
+  {:else if lobby.state === "PROMPT"}
+    <Prompt prompt={privatePlayer.prompt} uid={user.uid} {lobbyCode} />
   {:else}
-    unknown lobby state: {$lobby$.state}
+    unknown lobby state: {lobby.state}
   {/if}
 </div>
