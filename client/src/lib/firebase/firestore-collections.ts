4205c6f845a4300084e00fc45607b55e377b26f3
cmcclusky <cmcclus1@emich.edu>
2022-10-17 00:18:02 -0400

    feat: :sparkles: Further developement for voting.
    Needs testing of HTML and plenty more work to be closed to finished.

--- client/src/lib/firebase/firestore-collections.ts
+++ client/src/lib/firebase/firestore-collections.ts
@@ -1,8 +1,12 @@
-import { collection, type CollectionReference } from "firebase/firestore";
+import type { Lobby, PrivatePlayer } from "./firestore-types/lobby";
+import { collection, CollectionReference, DocumentReference } from "firebase/firestore";
 import { db } from "$lib/firebase/app";
 
-import type { Lobby } from "$lib/firebase/firestore-types/lobby";
 import type { UserData } from "./firestore-types/users";
 
 export const lobbyCollection = collection(db, "lobbies") as CollectionReference<Lobby>;
 export const userCollection = collection(db, "users") as CollectionReference<UserData>;
+
+export function getPrivatePlayerCollection(lobbyDoc: DocumentReference<Lobby>): CollectionReference<PrivatePlayer> {
+  return collection(lobbyDoc, "privatePlayers") as CollectionReference<PrivatePlayer>;
+}
