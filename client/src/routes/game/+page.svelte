96f13551495cffc6e665044847552d84a0ce74d9
njumaah <njumaah@emich.edu>
2022-10-17 16:00:18 -0400

    feat(client): ✨ add stalker sent to component
    Adds stalker being sent to their proper component in games page.svelte

--- client/src/routes/game/+page.svelte
+++ client/src/routes/game/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Prompt from "$components/Prompt.svelte";
   import LobbyComponent from "$components/Lobby.svelte";
+  import Stalker from "$components/Stalker.svelte";
 
   import { onSnapshot, doc, getDoc } from "firebase/firestore";
   import { onMount, onDestroy } from "svelte";
@@ -93,6 +94,8 @@
     Loading... <!-- TODO: make a Nice Loading spinner -->
   {:else if lobby.state === "PROMPT"}
     <Prompt prompt={privatePlayer.prompt} uid={$user.uid} {lobbyCode} />
+  {:else if lobby.state === "CHAT" && privatePlayer.stalker === true}
+    <Stalker {lobby} />
   {:else}
     unknown lobby state: {lobby.state}
   {/if}
