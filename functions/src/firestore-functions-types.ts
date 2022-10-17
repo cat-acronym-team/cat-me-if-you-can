596f736cb1833c27960b209d6f2bafd92edcf421
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-10-16 22:50:52 -0400

    refactor(functions): ♻️ use `typeof` and `in` to avoid `as` in `isLobbyRequest`
    this is a more idomatic way of checking

--- functions/src/firestore-functions-types.ts
+++ functions/src/firestore-functions-types.ts
@@ -4,5 +4,5 @@ export type ErrorResponse = { error?: string };
 
 export function isLobbyRequest(data: unknown): data is LobbyRequest {
   // will only return true if the data is an object with a code property and string
-  return data != null && (data as LobbyRequest).code !== undefined && typeof (data as LobbyRequest).code === "string";
+  return data != null && typeof data === "object" && "code" in data && typeof (data as LobbyRequest).code === "string";
 }
