1da5e5a1ff6e7cfdb340709dc3fdc726994073e0
gchamb <gchambe2@emich.edu>
2022-10-23 10:39:55 -0400

    feat(functions): ✨ add updated firebase admin

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -1,5 +1,11 @@
 import * as admin from "firebase-admin";
+import { AppOptions } from "firebase-admin";
+import * as functions from "firebase-admin/functions";
 
-admin.initializeApp();
+const firebaseConfig: AppOptions = {
+  serviceAccountId: "cat-me-if-you-can-game-dev@appspot.gserviceaccount.com",
+};
+const app = admin.initializeApp(firebaseConfig);
 
 export const db = admin.firestore();
+export const adminFunctions = functions.getFunctions(app);
