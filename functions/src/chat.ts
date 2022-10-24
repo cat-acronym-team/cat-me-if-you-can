5c100ebb3a8051c947f519f9a02fcf961003f24d
Giantte <gchambe2@emich.edu>
2022-10-24 11:04:35 -0400

    refactor(functions): ♻️ remove cloud task function, chat request type, and add expiration request

--- functions/src/chat.ts
+++ functions/src/chat.ts
@@ -1,22 +1,12 @@
 import { DocumentReference } from "firebase-admin/firestore";
-import * as functions from "firebase-functions";
 import { db } from "./app";
 import { getChatRoomCollection, getChatRoomMessagesCollection, getLobbyChatCollection } from "./firestore-collections";
-
 import { Lobby } from "./firestore-types/lobby";
 
-export const testTask = functions.tasks.taskQueue().onDispatch((data, context) => {
-  const promise = new Promise((resolve) => {
-    console.log("Hello World");
-    functions.logger.info("IN HERE BROTHER");
-  }) as Promise<void>;
-
-  return promise;
-});
 export function deleteChatRooms(lobbyDoc: DocumentReference<Lobby>) {
   db.runTransaction(async (transaction) => {
     const lobbyDocRef = (await transaction.get(lobbyDoc)).ref;
-    const chatRooms = await getChatRoomCollection(lobbyDoc).listDocuments();
+    const chatRooms = await getChatRoomCollection(lobbyDocRef).listDocuments();
 
     // get prompts answer then delete chatrooms
     const lobbyChatCollection = getLobbyChatCollection(lobbyDocRef);
@@ -33,47 +23,3 @@ export function deleteChatRooms(lobbyDoc: DocumentReference<Lobby>) {
     });
   });
 }
-// export const deleteChatRooms = functions.https.onCall(async (data: unknown, context) => {
-//   if (!context.auth) {
-//     return { error: "Not Signed In" };
-//   }
-//   if (!isLobbyRequest(data)) {
-//     return { error: "Invalid lobby code!" };
-//   }
-
-//   // get lobby doc, and check if the lobby exist
-//   const lobby = lobbyCollection.doc(data.code);
-//   const lobbyData = await lobby.get();
-//   if (!lobbyData.exists) {
-//     return { error: "Lobby doesn't exist!" };
-//   }
-//   // only allow the request coming from host or co-host to delete the chatrooms
-//   const { uids } = lobbyData.data() as Lobby;
-//   if (![uids[0], uids[1]].includes(context.auth.uid)) {
-//     return { error: "Not allowed to delete chatrooms" };
-//   }
-//   // delete all chatrooms
-//   const chatRooms = await getChatRoomCollection(lobby).listDocuments();
-//   // possibility that the host and co-host are deleting at the same time
-//   //  if one already deleted the chatrooms no need to try to delete them again
-//   if (chatRooms.length === 0) {
-//     return { error: "There's nothing to delete!" };
-//   }
-
-//   // Collect prompts answers from chatrooms
-//   const lobbyChatMessages = getLobbyChatCollection(lobby);
-//   for (const room of chatRooms) {
-//     // delete all of the messages within collection
-//     const messageCollection = getChatRoomMessagesCollection(room);
-//     const queryMessages = await messageCollection.orderBy("timestamp", "asc").get();
-
-//     // Add message to lobby messages
-//     // batch.create(lobbyChatMessages.add(queryMessages.docs[0]));
-//     // batch.create(lobbyChatMessages);
-
-//     // delete chatroom
-//     await room.delete();
-//   }
-//   // change game state
-//   return lobby.set({ state: "VOTE" }, { merge: true });
-// });
--- functions/src/firestore-functions-types.ts
+++ functions/src/firestore-functions-types.ts
@@ -1,27 +1,33 @@
+import { Timestamp } from "firebase-admin/firestore";
+
 export type LobbyRequest = { code: string };
 
 export type ErrorResponse = { error?: string };
+
+export type ExpirationRequest = {
+  time: Timestamp;
+  code: string;
+};
+
+export type GeneratedPairs = {
+  pairs: { one: string; two: string }[];
+  stalker?: string;
+};
 
 export function isLobbyRequest(data: unknown): data is LobbyRequest {
   // will only return true if the data is an object with a code property and string
   return data != null && typeof data === "object" && "code" in data && typeof (data as LobbyRequest).code === "string";
 }
 
-export type ChatRequest = { code: string; roomId: string; message: string };
-export function isChatRequest(data: unknown): data is ChatRequest {
+export function isExpirationRequest(data: unknown): data is ExpirationRequest {
   return (
     data != null &&
     typeof data === "object" &&
     "code" in data &&
-    typeof (data as ChatRequest).code === "string" &&
-    "roomId" in data &&
-    typeof (data as ChatRequest).roomId === "string" &&
-    "message" in data &&
-    typeof (data as ChatRequest).message === "string"
+    typeof (data as ExpirationRequest).code === "string" &&
+    "time" in data &&
+    typeof (data as ExpirationRequest).time === "object" &&
+    "seconds" in (data as ExpirationRequest).time &&
+    "nanoseconds" in (data as ExpirationRequest).time
   );
 }
-
-export type GeneratedPairs = {
-  pairs: { one: string; two: string }[];
-  stalker?: string;
-};
