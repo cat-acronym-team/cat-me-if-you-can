6fc6c87af2d1ba3e3ffbd03de6b8f9f3363ff86b
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-10-25 21:41:05 -0400

    refactor(client): 💄 tweek buttons

--- client/src/components/Lobby.svelte
+++ client/src/components/Lobby.svelte
@@ -45,12 +45,7 @@
     </div>
     <SelectAvatar {lobby} {lobbyCode} />
     <div class="start">
-      <Button
-        id="start-game"
-        on:click={async () => {
-          startGame({ code: lobbyCode });
-        }}><Label>Start Game</Label></Button
-      >
+      <Button id="start-game" on:click={() => startGame({ code: lobbyCode })}><Label>Start Game</Label></Button>
     </div>
     <div class="buttons">
       <h3 class="invite-link">Invite Link: {url}</h3>
