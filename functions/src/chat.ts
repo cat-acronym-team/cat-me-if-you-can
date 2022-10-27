44406271bd8f7272e79f89b61c8e1599f206dc3b
gchamb <gchambe2@emich.edu>
2022-10-27 15:44:29 -0400

    fix(functions): 🐛 return transaction and promise all

--- functions/src/chat.ts
+++ functions/src/chat.ts
@@ -1,25 +1,47 @@
 import type { DocumentReference } from "firebase-admin/firestore";
 import { db } from "./app";
-import { getChatRoomCollection, getChatRoomMessagesCollection, getLobbyChatCollection } from "./firestore-collections";
+import { getChatRoomCollection, getChatRoomMessagesCollection } from "./firestore-collections";
 import { Lobby } from "./firestore-types/lobby";
 
-export function deleteChatRooms(lobbyDoc: DocumentReference<Lobby>) {
-  db.runTransaction(async (transaction) => {
-    const lobbyDocRef = (await transaction.get(lobbyDoc)).ref;
-    const chatRooms = await getChatRoomCollection(lobbyDocRef).listDocuments();
+export async function deleteChatRooms(lobbyDoc: DocumentReference<Lobby>) {
+  const lobbyDocSnapshot = await lobbyDoc.get();
+  return db.runTransaction(async () => {
+    const chatRooms = await getChatRoomCollection(lobbyDoc).listDocuments();
 
-    // get prompts answer then delete chatrooms
-    const lobbyChatCollection = getLobbyChatCollection(lobbyDocRef);
-    chatRooms.forEach(async (room) => {
-      // order the messages from the chatroom
-      const orderMessages = await getChatRoomMessagesCollection(room).orderBy("timestamp", "asc").get();
-      // get the first two messages which are the prompt answers and add to lobby chat
-      lobbyChatCollection.add(orderMessages.docs[0].data());
-      lobbyChatCollection.add(orderMessages.docs[1].data());
-      // delete messages
-      orderMessages.docs.forEach((m) => m.ref.delete());
-      // then delete room
-      room.delete();
-    });
+    Promise.all(
+      chatRooms.map(async (room) => {
+        // get the messages from the chatroom
+        const messages = await getChatRoomMessagesCollection(room).get();
+        // get the prompt answers from the messages
+        const answers = new Map<string, string>();
+        messages.forEach((messageDoc) => {
+          // get message info
+          const message = messageDoc.data();
+          // if the message is the prompt answer then add to map
+          if (message.isPromptAnswer) {
+            answers.set(message.sender, message.text);
+          }
+          // delete all messages
+          messageDoc.ref.delete();
+        });
+
+        // add the prompt answers to their player object
+        const { players, uids } = lobbyDocSnapshot.data() as Lobby;
+        for (const [key, value] of answers) {
+          // get index and add their prompt answer to their player object
+          const playerIndex = uids.indexOf(key);
+          const player = players[playerIndex];
+          player.promptAnswer = value;
+          // add back to the original array
+          players[playerIndex] = player;
+        }
+
+        // updating the players with the new players array with their answers
+        lobbyDoc.update({ players });
+
+        // then delete room
+        room.delete();
+      })
+    );
   });
 }
