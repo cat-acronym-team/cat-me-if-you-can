1d40a35cca860684b7e56ff9cca565f3c7895254
gchamb <gchambe2@emich.edu>
2022-10-27 15:47:38 -0400

    refactor(functions): ♻️ relocate pairs type and minor refactor

--- functions/src/util.ts
+++ functions/src/util.ts
@@ -1,12 +1,16 @@
-import { GeneratedPairs } from "./firestore-functions-types";
 import { Lobby } from "./firestore-types/lobby";
+
+export type GeneratedPairs = {
+  pairs: { one: string; two: string }[];
+  stalker?: string;
+};
 
 export function generatePairs(lobbyData: Lobby): GeneratedPairs {
   const pairs: GeneratedPairs["pairs"] = [];
   let stalker: string | undefined;
 
   // get only the uids of alive players
-  let aliveUids: string[] = [];
+  const aliveUids: string[] = [];
   lobbyData.players.forEach((player, playerIndex) => {
     if (player.alive) {
       aliveUids.push(lobbyData.uids[playerIndex]);
@@ -17,7 +21,7 @@ export function generatePairs(lobbyData: Lobby): GeneratedPairs {
   while (aliveUids.length > 1) {
     // get random pair
     const one = aliveUids[Math.floor(Math.random() * aliveUids.length)];
-    let two = aliveUids[Math.floor(Math.random() * aliveUids.length)];
+    let two: string;
     // generate a random player if the current pairs are equal
     do {
       two = aliveUids[Math.floor(Math.random() * aliveUids.length)];
@@ -25,9 +29,8 @@ export function generatePairs(lobbyData: Lobby): GeneratedPairs {
     // organize the pairs
     pairs.push({ one, two });
     // delete them from array
-    aliveUids = aliveUids.filter((uid) => {
-      return ![one, two].includes(uid);
-    });
+    aliveUids.splice(aliveUids.indexOf(one), 1);
+    aliveUids.splice(aliveUids.indexOf(two), 1);
   }
 
   // Make this guy a stalker somehow
