f8f3156fb4aca7309592fc67a01593343b067fd5
njumaah <njumaah@emich.edu>
2022-11-01 10:36:40 -0400

    feat(client): ✨ add findViewerChatRoom function
    add function that allows for the search for viewers in a chatroom

--- client/src/lib/firebase/chat.ts
+++ client/src/lib/firebase/chat.ts
@@ -8,3 +8,10 @@ export async function findChatRoom(lobbyId: string, playerId: string) {
   const queryChatRoom = await getDocs(query(getChatRoomCollection(lobbyId), where("pair", "array-contains", playerId)));
   return queryChatRoom.docs[0];
 }
+
+export async function findViewerChatRoom(lobbyId: string, viewerId: string) {
+  const queryChatRoom = await getDocs(
+    query(getChatRoomCollection(lobbyId), where("viewers", "array-contains", viewerId))
+  );
+  return queryChatRoom.docs[0];
+}
