b9c7d3c03a1afa09bb7a2f7cefe84f91d7bb75f8
zaverbac <61805664+zaverbac@users.noreply.github.com>
2022-11-03 13:12:40 -0400

    refactor(client): ♻️ Remove unneccessary functionality, tags
    Remove unused initialfocus function, style tag, and ID

--- client/src/routes/settings/+page.svelte
+++ client/src/routes/settings/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { deleteAccount, logOut } from "$lib/firebase/auth";
   import { goto } from "$app/navigation";
-  import Dialog, { Title, Content, Actions, InitialFocus } from "@smui/dialog";
+  import Dialog, { Title, Content, Actions } from "@smui/dialog";
   import Button, { Label } from "@smui/button";
 
   let showDeletionPrompt = false;
@@ -35,7 +35,7 @@
       <Title id="confirm-account-deletion-title">Delete Account</Title>
       <Content id="deletion-confirmation-content">Are you sure?</Content>
       <Actions>
-        <Button id="cancel">
+        <Button>
           <Label>Cancel</Label>
         </Button>
         <Button on:click={verifyDelete}>
@@ -49,12 +49,10 @@
         <Title id="reauth-title">NOTICE!</Title>
         <Content id="err-msg-content"
           >Last sign in too long ago.
-          <br />Click ok to be redirected back to sign in</Content
+          <br />Please Signin and Try Again</Content
         >
         <Actions>
           <Button
-            defaultAction
-            use={[InitialFocus]}
             on:click={() => {
               logOut();
               goto("/");
@@ -70,6 +68,3 @@
     </Button>
   </main>
 </html>
-
-<style>
-</style>
