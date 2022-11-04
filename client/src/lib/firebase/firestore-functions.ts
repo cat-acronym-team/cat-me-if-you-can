086bb3f522683f6f384f97419b0635fe4df48425
njumaah <njumaah@emich.edu>
2022-11-04 13:44:28 -0400

    refactor(client): 🔥 remove deleteChatRooms
    remove deleteChatRooms in firestore-functions

--- client/src/lib/firebase/firestore-functions.ts
+++ client/src/lib/firebase/firestore-functions.ts
@@ -6,7 +6,6 @@ import type { ChangeAvatarData } from "./functions-types/avatar";
 export const startGame = httpsCallable<LobbyRequest, void>(functions, "startGame");
 export const joinLobby = httpsCallable<LobbyRequest, void>(functions, "joinLobby");
 export const stalkChatroom = httpsCallable<StalkChatroomRequest, void>(functions, "stalkChatroom");
-export const deleteChatRooms = httpsCallable<LobbyRequest, void>(functions, "deleteChatRooms");
 export const addChatMessage = httpsCallable<ChatRequest, void>(functions, "addChatMessage");
 export const changeAvatar = httpsCallable<ChangeAvatarData, void>(functions, "changeAvatar");
 export const verifyExpiration = httpsCallable<LobbyRequest, void>(functions, "verifyExpiration");
