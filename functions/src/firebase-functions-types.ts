b50d9c2725428673e0a9550722d5382936f17281
Muaad2001 <98790792+Muaad2001@users.noreply.github.com>
2022-11-08 09:54:59 -0500

    fix(client): 🔥 removed getLobbyChatCollection in chat.ts

--- functions/src/firebase-functions-types.ts
+++ functions/src/firebase-functions-types.ts
@@ -1,6 +1,6 @@
 export type LobbyRequest = { code: string };
-
 export type LobbyCreationResponse = { code: string };
+
 export function isLobbyRequest(data: unknown): data is LobbyRequest {
   // will only return true if the data is an object with a code property and string
   return data != null && typeof data === "object" && "code" in data && typeof (data as LobbyRequest).code === "string";
