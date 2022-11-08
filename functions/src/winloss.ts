f62322fdc4acca3623ea9cc3d1a50c0199286cf6
Giantte <gchambe2@emich.edu>
2022-11-08 16:46:28 -0500

    refactor(functions): ♻️ delete vote docs no matter the winner result

--- functions/src/winloss.ts
+++ functions/src/winloss.ts
@@ -3,7 +3,6 @@ import * as functions from "firebase-functions";
 import { db } from "./app";
 import {
   getPrivatePlayerCollection,
-  getVoteCollection,
   lobbyCollection,
   userCollection,
 } from "./firestore-collections";
@@ -54,9 +53,6 @@ export async function endGameProcess(
   let { players } = lobbyData;
   const { uids, winner } = lobbyData;
 
-  const voteCollection = getVoteCollection(lobbyDocRef);
-  const voteDocs = await transaction.get(voteCollection); // reading before for transaction issues
-
   // update each players doc
   await Promise.all(
     uids.map(async (uid, index) => {
@@ -101,11 +97,6 @@ export async function endGameProcess(
   players = players.map(({ role, ...rest }) => {
     return { ...rest, alive: true, votes: 0 };
   });
-
-  // delete vote documents
-  for (const voteDoc of voteDocs.docs) {
-    transaction.delete(voteDoc.ref);
-  }
 
   // create a WriteBatch
   const batch = db.batch();
