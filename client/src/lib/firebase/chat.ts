815656a5853e8edb24eddeb710f920aaf8e417ab
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-11-08 19:42:23 -0500

    refactor(functions): 🔥 remove unused functions

--- client/src/lib/firebase/chat.ts
+++ client/src/lib/firebase/chat.ts
@@ -1,21 +1,7 @@
-import { query, where, getDocs, addDoc, serverTimestamp } from "firebase/firestore";
-import { getChatRoomCollection, getChatRoomMessagesCollection } from "./firestore-collections";
+import { addDoc, serverTimestamp } from "firebase/firestore";
+import { getChatRoomMessagesCollection } from "./firestore-collections";
 import { chatMessageValidator } from "./firestore-types/lobby";
 
-/**
- * Checks if the user is in a chatroom then returns their chatroom doc
- */
-export async function findChatRoom(lobbyId: string, playerId: string) {
-  const queryChatRoom = await getDocs(query(getChatRoomCollection(lobbyId), where("pair", "array-contains", playerId)));
-  return queryChatRoom.docs[0];
-}
-
-export async function findViewerChatRoom(lobbyId: string, viewerId: string) {
-  const queryChatRoom = await getDocs(
-    query(getChatRoomCollection(lobbyId), where("viewers", "array-contains", viewerId))
-  );
-  return queryChatRoom.docs[0];
-}
 /**
  * Adds Message for the user to the chatroom
  */
