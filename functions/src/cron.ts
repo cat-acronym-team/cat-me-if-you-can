5c6062f3eb2d416e1abeab1779ca38735d69dbdf
gchamb <gchambe2@emich.edu>
2022-11-15 15:17:08 -0500

    refactor(functions): ♻️ change variable name

--- functions/src/cron.ts
+++ functions/src/cron.ts
@@ -12,11 +12,11 @@ export const testLogic = functions.https.onRequest(async (req, res) => {
 // export const deleteOldLobbies = functions.pubsub.schedule("15 3 * * *").onRun(async (context) => {
 const deleteOldLobbies = async () => {
   // TODO: Remove if not testing the logic
-  const dayFromTodaySecs = firestore.Timestamp.now().seconds - 86_400; // A day ago from today in seconds
-  const dayFromTodayDate = firestore.Timestamp.fromDate(new Date(dayFromTodaySecs * 1000)); // a day ago from today in date object
+  const oneDayAgoAsSecs = firestore.Timestamp.now().seconds - 24 * 60 * 60; // A day ago from today in seconds
+  const oneDayAgoAsDate = firestore.Timestamp.fromDate(new Date(oneDayAgoAsSecs * 1000)); // a day ago from today in date object
 
   // query all lobby documents that expiration date is either less than or equal from 24 hours from today
-  const oldLobbies = await db.collection("/lobbies").where("expiration", "<=", dayFromTodayDate).get();
+  const oldLobbies = await db.collection("/lobbies").where("expiration", "<=", oneDayAgoAsDate).get();
 
   if (oldLobbies.empty) {
     return;
