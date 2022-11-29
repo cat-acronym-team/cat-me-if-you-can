1f0ddede85350f745738ec0803b4db7ead6328d0
njumaah <njumaah@emich.edu>
2022-11-29 09:28:09 -0500

    refactor(client): ♻️ make lobbyCode not undefined
    when sent to the playermenu, lobbyCode is already checked to be not undefined

--- client/src/components/PlayerMenu.svelte
+++ client/src/components/PlayerMenu.svelte
@@ -7,7 +7,7 @@
   // variables
   let menu: Menu;
 
-  export let lobbyCode: string | undefined = undefined;
+  export let lobbyCode: string;
   export let uid: string | undefined = undefined;
 </script>
 
@@ -15,10 +15,10 @@
   <IconButton class="material-icons" on:click={() => menu.setOpen(true)}>more_vert</IconButton>
   <Menu bind:this={menu}>
     <List>
-      <Item on:SMUI:action={() => kick({ code: lobbyCode ?? "", uid: uid ?? "" })}>
+      <Item on:SMUI:action={() => kick({ code: lobbyCode, uid: uid ?? "" })}>
         <Text>Kick</Text>
       </Item>
-      <Item on:SMUI:action={() => ban({ code: lobbyCode ?? "", uid: uid ?? "" })}>
+      <Item on:SMUI:action={() => ban({ code: lobbyCode, uid: uid ?? "" })}>
         <Text>Ban</Text>
       </Item>
     </List>
--- client/src/components/SelectAvatar.svelte
+++ client/src/components/SelectAvatar.svelte
@@ -66,7 +66,7 @@
 <div class="grid {lobby != undefined ? 'lobby' : ''}">
   {#each avatarChoices as { avatar, altText, displayName, uid, available, selected }}
     <div class="parent">
-      {#if !available && $user !== null && lobby?.uids[0] == $user.uid}
+      {#if !available && lobbyCode != undefined && $user !== null && lobby?.uids[0] == $user.uid}
         <PlayerMenu {lobbyCode} {uid} />
       {/if}
       <button class="avatar" on:click={() => selectAvatar(avatar)} disabled={!available} aria-selected={selected}>
