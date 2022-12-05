ffca0df0dde26b0d799875b7a499621eab2dcfd6
mrjoshua520 <joshua52099@gmail.com>
2022-12-04 19:37:20 -0500

    refactor(client): 🎨 Refactor game page.svelte

--- client/src/components/SelectAvatar.svelte
+++ client/src/components/SelectAvatar.svelte
@@ -62,7 +62,7 @@
 <div class="grid {lobby != undefined ? 'lobby' : ''}">
   {#each avatarChoices as { avatar, altText, displayName, uid, available, selected }}
     <div class="parent">
-      {#if uid != undefined && lobbyCode != undefined && $user !== null && lobby?.uids[0] == $user.uid}
+      {#if uid != undefined && lobbyCode != undefined && $user !== null && lobby?.host == $user.uid}
         <PlayerMenu {lobbyCode} {uid} />
       {/if}
       <button class="avatar" on:click={() => selectAvatar(avatar)} disabled={!available} aria-selected={selected}>
