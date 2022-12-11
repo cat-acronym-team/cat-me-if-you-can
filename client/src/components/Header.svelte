b7b791a72fadcc910376a75ba77e136e292fd6a8
mrjoshua520 <joshua52099@gmail.com>
2022-12-11 00:53:54 -0500

    feat(client): 💄 Change header to be it's own file

---
+++ client/src/components/Header.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Rules from "$components/Rules.svelte";
+</script>
+
+<header class="header">
+  <Rules />
+  <slot name="top-right" />
+</header>
+
+<style>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: absolute;
+    top: 8px;
+    left: 8px;
+    right: 8px;
+  }
+</style>
--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,12 +1,6 @@
 <script lang="ts">
   import "@material/typography/mdc-typography.scss";
-  import Rules from "$components/Rules.svelte";
 </script>
-
-<header class="header">
-  <Rules />
-  <slot name="top-right" />
-</header>
 
 <slot />
 
@@ -15,16 +9,5 @@
     margin: 0;
     height: 100vh;
     height: 100dvh;
-  }
-
-  .header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    position: absolute;
-    gap: 8px;
-    top: 8px;
-    left: 8px;
-    right: 8px;
   }
 </style>
