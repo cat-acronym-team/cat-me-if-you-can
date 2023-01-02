ed1fdd4c32b56734b37bc2c7a7cb0310104437fb
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2023-01-02 12:05:08 -0500

    feat(client): ✨ add custom 404 page

---
+++ client/src/components/ErrorPage.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  export let heading: string;
+  export let body: string;
+</script>
+
+<svelte:head>
+  <!-- tell google search not to index error pages -->
+  <meta name="robots" content="noindex" />
+</svelte:head>
+
+<main>
+  <h1 class="mdc-typography--headline1">{heading}</h1>
+  <p class="mdc-typography--body1">{body}</p>
+  <slot name="button" />
+</main>
+
+<style>
+  main {
+    box-sizing: border-box;
+    min-height: 100dvh;
+    padding: 24px;
+    display: grid;
+    justify-items: center;
+    align-content: center;
+    text-align: center;
+  }
+
+  h1 {
+    margin-top: 0;
+  }
+</style>
